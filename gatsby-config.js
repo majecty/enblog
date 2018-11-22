109fe6b7dc24a00f27a72754291475e146722f31
Park Juhyung <majecty@gmail.com>
2018-11-22 23:11:26 +0900

    Fix post link in rss

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
       options: {
         name: `Juhyung's blog`,
         short_name: `Juhyung's blog`,
-        start_url: '/',
+        start_url: '/enblog',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
@@ -52,7 +52,58 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-feed`
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+              pathPrefix
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  url: site.siteMetadata.siteUrl + site.pathPrefix + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl+ site.pathPrefix + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 1000,
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "JuhyungBlog RSS Feed",
+          },
+        ],
+      },
     },
     `gatsby-plugin-sitemap`,
     {
